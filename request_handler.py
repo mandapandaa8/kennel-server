88ce42b0aa266f254cf918c1e4954ba50b55c43b
Amanda De Leon <mandapanda6991@yahoo.com>
2022-10-11 15:00:10 -0500

    worked with status codes for the GET and POST

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,9 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals
+from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
+from views import get_all_locations, get_single_location, create_location, delete_location, update_location
+from views import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
+from views import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
 
 
 # Here's a class. It inherits from another class.
@@ -9,50 +11,198 @@ from views import get_all_animals
 # work together for a common purpose. In this case, that
 # common purpose is to respond to HTTP requests from a client.
 class HandleRequests(BaseHTTPRequestHandler):
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            # Convert the string "1" to the integer 1
+            # This is the new parseInt()
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
     # This is a Docstring it should be at the beginning of all classes and functions
     # It gives a description of the class or function
-    """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
-    """
+    """Controls the functionality of any GET, PUT, POST, DELETE requests to the server"""
 
     # Here's a class function
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
-        """Handles GET requests to the server
-        """
-        # Set the response code to 'Ok'
-        self._set_headers(200)
 
-        # Your new console.log() that outputs to the terminal
-        print(self.path)
+        # Parse the URL and capture the tuple that is returned
+        (resource, id) = self.parse_url(self.path)
 
-        # It's an if..else statement
-        if self.path == "/animals":
-            response = get_all_animals()
-        else:
-            response = []
+        if resource == "animals":
+            if id is not None:
+                response = get_single_animal(id)
+                if response is not None:
+                    self._set_headers(200)
+                else:
+                    self._set_headers(404)
+                    response = { "message": f"Animal {id} is out playing right now" }
+            else:
+                self._set_headers(200)
+                response = get_all_animals()
 
-        # Send a JSON formatted string as a response
+        if resource == "locations":
+            if id is not None:
+                response = get_single_location(id)
+                if response is not None:
+                    self._set_headers(200)
+                else:
+                    self._set_headers(404)
+                    response = { "message": f"Animal {id} is out playing right now" }
+            else:
+                self._set_headers(200)
+                response = get_all_locations()
+
+        if resource == "employees":
+            if id is not None:
+                response = get_single_employee(id)
+                if response is not None:
+                    self._set_headers(200)
+                else:
+                    self._set_headers(404)
+                    response = { "message": f"Animal {id} is out playing right now" }
+            else:
+                self._set_headers(200)
+                response = get_all_employees()
+
+        if resource == "customers":
+            if id is not None:
+                response = get_single_customer(id)
+                if response is not None:
+                    self._set_headers(200)
+                else:
+                    self._set_headers(404)
+                    response = { "message": f"Animal {id} is out playing right now" }
+            else:
+                response = get_all_customers()
+
         self.wfile.write(json.dumps(response).encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server"""
-
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = { "payload": post_body }
-        self.wfile.write(json.dumps(response).encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_object = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            if "name" in post_body and "species" in post_body and "locationId" in post_body and "customerId" in post_body and "status" in post_body:
+                self._set_headers(201)
+                new_object = create_animal(post_body)
+            else:
+                self._set_headers(400)
+                new_object = {
+                    "message": f'{"name is required" if "name" not in post_body else ""} {"species is required" if "species" not in post_body else ""} {"status is required" if "status" not in post_body else ""}'
+                }
+                
+
+        if resource == "locations":
+            if "name" in post_body and "address" in post_body:
+                self._set_headers(201)
+                new_object = create_location(post_body)
+            else:
+                self._set_headers(400)
+                new_object = {
+                    "message": f'{"name is required" if "name" not in post_body else ""} {"address is required" if "address" not in post_body else ""}'
+                }
+
+        if resource == "employees":
+            if "name" in post_body:
+                self._set_headers(201)
+                new_object = create_employee(post_body)
+            else:
+                self._set_headers(400)
+                new_object = {
+                    "message": f'{"name is required" if "name" not in post_body else ""}'
+                }
+
+        if resource == "customers":
+            if "name" in post_body:
+                self._set_headers(201)
+                new_object = create_customer(post_body)
+            else:
+                self._set_headers(400)
+                new_object = {
+                    "message": f'{"name is required" if "name" not in post_body else ""}'
+                }
+
+        # Encode the new animal and send in response
+        self.wfile.write(json.dumps(new_object).encode())
+
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        if resource == "locations":
+            delete_location(id)
+
+        if resource == "employees":
+            delete_employee(id)
+
+        if resource == "customers":
+            delete_customer(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     # A method that handles any PUT request.
     def do_PUT(self):
-        """Handles PUT requests to the server"""
-        self.do_PUT()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        if resource == "locations":
+            update_location(id, post_body)
+
+        if resource == "employees":
+            update_employee(id, post_body)
+        
+        if resource == "customers":
+            update_customer(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def _set_headers(self, status):
         # Notice this Docstring also includes information about the arguments passed to the function
